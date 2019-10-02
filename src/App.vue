fc7927233e181b3cd387bb4416f221076c6ea7ed
Anthony Jackman <energeticpixels@gmail.com>
2019-10-01 21:29:48 -0500

    initial project - no parcel bundler yet

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class='container'>
+    <h1>{{ message }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Using parcel in a vue.js (version 2) app'
+    };
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    width: 600px;
+    margin: 50px auto;
+    text-align: center;
+  }
+</style>
